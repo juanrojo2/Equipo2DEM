c67ae257147de174d15a978f7f6d8cf38b1b2579
Juan José Rojo <56183471+juanrojo2@users.noreply.github.com>
2020-09-21 16:03:56 -0500

    Cambios minimos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import $ from "jquery";
-import "popper.js";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.render(
   <React.StrictMode>
