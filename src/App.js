e94ffba3763728e03efc03b8a8360e2b08b15e7a
Juan José Rojo <56183471+juanrojo2@users.noreply.github.com>
2020-09-22 11:15:09 -0500

    Componentes reiniciados

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Nav from "./components/nav";
 
 export default class App extends React.Component {
   render() {
-    return (
-      <div>
-        <Nav />
-      </div>
-    );
+    return <div></div>;
   }
 }
