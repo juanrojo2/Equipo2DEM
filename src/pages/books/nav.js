b6471c9c640c60c07428e2ecfbc321c027f44ba0
Juan José Rojo <56183471+juanrojo2@users.noreply.github.com>
2020-10-16 21:42:20 -0500

    Agregada barra de navegación libros

---
+++ src/pages/books/nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class NavComentarios extends React.Component {
+  render() {
+    return (
+      <nav class="navbar sticky-top navbar-light bg-light">
+        <button type="button" class="btn btn-outline-dark btn-lg">
+          Comentarios y notas
+        </button>
+      </nav>
+    );
+  }
+}
