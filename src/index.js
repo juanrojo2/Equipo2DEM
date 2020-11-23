87f67fc5421b1620787b3c03b75d33598f486654
Juan José Rojo <56183471+juanrojo2@users.noreply.github.com>
2020-11-23 15:11:48 -0500

    Cambios menores

--- src/index.js
+++ src/index.js
@@ -1,33 +1,22 @@
 const express = require("express");
-
 const cors = require("cors");
-//const sha1 = require("sha1");
-// const cookieParser = require("cookie-parser");
-
-let port = process.env.PORT || 5000;
-
 const routes = require("./routes/routes");
 
 const app = express();
 
+app.set("port", process.env.PORT || 3001);
+
 app.use(express.json());
 
-//app.use(cors({origin: '*'}));
-app.use(cors());
-app.use(express.json());
+app.use(cors({ origin: "*" }));
+//app.use(cors({origin: 'https://kuepj-3000.sse.codesandbox.io'}));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); //acepta peticiones desde cualquier sitio
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT,POST,OPTIONS,PATCH,GET,DELETE"
-  ); //acepta los metodos CRUD
-  res.setHeader("Content-Type", "application/json;charset=utf-8"); //acepta datps tipo JSON y caracteres especiales
-  next();
+app.get("/", (req, res) => {
+  res.send("hi");
 });
 // Routes//
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
+app.listen(app.get("port"), () => {
+  console.log(`running server port 3001`);
 });
