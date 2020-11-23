74fabb2d312c27d5e9254f7c1c169bd78d576464
Jaxon Rodriguez Cardona <66315214+JaxonRodriguezC@users.noreply.github.com>
2020-11-22 20:09:57 -0500

    Pagina Notas Agregada pagina notas

--- src/pages/PagComentarios.js
+++ src/pages/PagComentarios.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Nav/navBar";
 import "../styles/styles.css";
 import styled from "styled-components";
+import { Modal, ModalHeader, ModalBody } from "reactstrap";
 
 const H1 = styled.h1`
   font-family: "Lobster", cursive;
@@ -15,24 +16,73 @@ const H1 = styled.h1`
   }
 `;
 
-const PagComentarios = () => {
+class PagComentarios extends React.Component {
+  state = {
+    abierto: false
+  };
+  abrirModal = () => {
+    this.setState({ abierto: !this.state.abierto });
+  };
+  render() {
     return (
-      <div>
-        <Navbar />
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <H1 className="text-center title mt-5">Comentarios</H1>
-              <br />
-              <h3 className="text-center mt-5">
-                ¡Esta un poco vacio aqui! Agrega comentarios de tus libros
-                favoritos y apareceran aqui.
-              </h3>
+      <>
+        <div>
+          <Navbar />
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <H1 className="text-center title mt-5">Comentarios</H1>
+                <br />
+                <div className="card">
+                  <div className="card-body text-center div-card">
+                    This is some text within a card body.
+                    <hr />
+                    <div className="div-btn-com">
+                      <button
+                        type="button"
+                        className="btn btn-outline-dark btn-block"
+                        onClick={this.abrirModal}
+                      >
+                        Comentario
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+
+        <div>
+          <Modal isOpen={this.state.abierto}>
+            <ModalHeader>Crear Comentario</ModalHeader>
+            <ModalBody>
+              <form>
+                <div className="form-group">
+                  <label for="comment">Comentario</label>
+                  <input type="text" className="form-control" id="comment" />
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-outline-primary btn-block"
+                  onClick={this.abrirModal}
+                >
+                  Añadir comentario
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-outline-danger btn-block"
+                  onClick={this.abrirModal}
+                >
+                  Cerrar
+                </button>
+              </form>
+            </ModalBody>
+          </Modal>
+        </div>
+      </>
     );
+  }
 }
 
 export default PagComentarios;
--- src/pages/PagNotas.js
+++ src/pages/PagNotas.js
@@ -2,24 +2,75 @@ import React from "react";
 import Navbar from "../components/Nav/navBar";
 import "../styles/styles.css";
 
-const PagNotas = () => {
+import { Modal, ModalHeader, ModalBody } from "reactstrap";
+
+class PagNotas extends React.Component {
+  state = {
+    abierto: false
+  };
+  abrirModal = () => {
+    this.setState({ abierto: !this.state.abierto });
+  };
+  render() {
     return (
-      <div>
-        <Navbar />
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <h1 className="text-center title mt-5">Notas</h1>
-              <br />
-              <h3 className="text-center mt-5">
-                ¡Esta un poco vacio aqui! Toma notas de tus libros favoritos y
-                apareceran aqui.
-              </h3>
+      <>
+        <div>
+          <Navbar />
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h1 className="text-center title mt-5">Notas</h1>
+                <br />
+                <div className="card">
+                  <div className="card-body text-center div-card">
+                    This is some text within a card body.
+                    <hr />
+                    <div className="div-btn">
+                      <button
+                        type="button"
+                        className="btn btn-outline-dark btn-block"
+                        onClick={this.abrirModal}
+                      >
+                        Nota
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+
+        <div>
+          <Modal isOpen={this.state.abierto}>
+            <ModalHeader>Crear Comentario</ModalHeader>
+            <ModalBody>
+              <form>
+                <div className="form-group">
+                  <label for="comment">Comentario</label>
+                  <input type="text" className="form-control" id="comment" />
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-outline-primary btn-block"
+                  onClick={this.abrirModal}
+                >
+                  Añadir comentario
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-outline-danger btn-block"
+                  onClick={this.abrirModal}
+                >
+                  Cerrar
+                </button>
+              </form>
+            </ModalBody>
+          </Modal>
+        </div>
+      </>
     );
+  }
 }
 
 export default PagNotas;
