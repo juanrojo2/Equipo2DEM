fb434653e096037e030ba1dcc7f870b91834bd51
Jaxon Rodriguez Cardona <66315214+JaxonRodriguezC@users.noreply.github.com>
2020-11-23 11:55:02 -0500

    Querys Corregidos errores de querys

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,7 +8,7 @@ router.post("/SingUp/", (req, res) => {
   const { name, userEmail, password } = req.body;
   //const abreviatura = req.body.mod
   mysqlConnection.query(
-    `INSERT INTO USUARIO (NOMBRE_COMPLETO, CORREO_ELECTRONICO, CONTRASENA) VALUES ('${name},${userEmail}', '${password}')`,
+    `INSERT INTO USUARIO (NOMBRE_COMPLETO, CORREO_ELECTRONICO, CONTRASENA) VALUES ('${name}, ${userEmail}', '${password}')`,
     (err, rows, fields) => {
       if (!err) {
         res.json({ message: "Usuario registrado" });
@@ -24,7 +24,7 @@ router.post("/Login/", (req, res) => {
   //const abreviatura = req.body.mod
   console.log(userEmail, password);
   mysqlConnection.query(
-    `SELECT * FROM USUARIOS WHERE CORREO_ELECTRONICO='${userEmail}' AND CONTRASENA='${password}'`,
+    `SELECT * FROM USUARIO WHERE CORREO_ELECTRONICO='${userEmail}' AND CONTRASENA='${password}'`,
     (err, rows, fields) => {
       if (!err) {
         res.json({ message: "Bienvenido" });
